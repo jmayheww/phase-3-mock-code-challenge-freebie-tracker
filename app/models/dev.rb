d5d7d9d7986eb70487818c33211e2d8fb4968ba4
Joshua <joshsmayhew@gmail.com>
2022-12-17 15:41:46 +0700

    commit tree dd0a5a39f0c2559724a407b40dd174aa9986eb8c

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,22 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    has_item = freebies.filter { |freebie| freebie.item_name == item_name }
+
+    if has_item.length.positive?
+      true
+    else
+      false
+    end
+  end
+
+  def give_away(dev:, freebie:)
+    if freebie.dev_id == id
+      freebie.update(dev_id: dev.id)
+    else
+      puts 'nah'
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,31 @@
-puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+puts 'Creating companies...'
+Company.delete_all
+Dev.delete_all
+Freebie.delete_all
+c1 = Company.create(name: 'Google', founding_year: 1998)
+c2 = Company.create(name: 'Facebook', founding_year: 2004)
+c3 = Company.create(name: 'Dunder Mifflin', founding_year: 2002)
+c4 = Company.create(name: 'Enron', founding_year: 1995)
 
-puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+puts 'Creating devs...'
+d1 = Dev.create(name: 'Rick')
+d2 = Dev.create(name: 'Morty')
+d3 = Dev.create(name: 'Mr. Meseeks')
+d4 = Dev.create(name: 'Gazorpazop')
 
-puts "Creating freebies..."
+puts 'Creating freebies...'
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: 'Pizza', value: 42, company_id: c1.id, dev_id: d1.id)
+Freebie.create(item_name: 'Burgers', value: 68, company_id: c2.id, dev_id: d2.id)
+Freebie.create(item_name: 'Job', value: 9999, company_id: c1.id, dev_id: d2.id)
+Freebie.create(item_name: 'Water Bottle', value: 10, company_id: c3.id, dev_id: d1.id)
+Freebie.create(item_name: 'Subscription', value: 80, company_id: c4.id, dev_id: d3.id)
+Freebie.create(item_name: 'Pen', value: 5, company_id: c3.id, dev_id: d4.id)
+Freebie.create(item_name: 'Mousepad', value: 54, company_id: c1.id, dev_id: d1.id)
 
-puts "Seeding done!"
+puts 'Seeding done!'
